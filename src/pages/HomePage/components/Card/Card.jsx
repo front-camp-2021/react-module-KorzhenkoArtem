8ef1d19ff50619ca1ddfe83ebbe8750bf5c7767a
KorzhenkoArtem <34282458+KorzhenkoArtem@users.noreply.github.com>
2021-11-27 22:29:32 +0200

    wip

--- src/pages/HomePage/components/Card/Card.jsx
+++ src/pages/HomePage/components/Card/Card.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-// TODO: add rating
 const Card = (props) => {
   const addToCartHandler = () => {
     alert(`product with id ${props.id} should be added to cart`);
@@ -10,29 +9,29 @@ const Card = (props) => {
   };
 
   return (
-      <div className="card-container-inner">
-        <div className="card-top">
-          <div className="card-image">
-            <img src={props.image} alt={props.title} />
-          </div>
-          <div className="rate-and-price">
-            <span className="rate">{props.rating}</span>
-            <span className="price">${props.price}</span>
-          </div>
-          <div className="card-description">
-            <h4>{props.title}</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-          </div>
+    <div className="card-container-inner">
+      <div className="card-top">
+        <div className="card-image">
+          <img src={props.image} alt={props.title} />
         </div>
-        <div className="card-bottom">
-          <button className="wishlist" onClick={addToWishlistHandler}>
-            <span>Wishlist</span>
-          </button>
-          <button className="add-to-cart" onClick={addToCartHandler}>
-            <span>Add to cart</span>
-          </button>
+        <div className="rate-and-price">
+          <span className="rate">{props.rating}</span>
+          <span className="price">${props.price}</span>
+        </div>
+        <div className="card-description">
+          <h4>{props.title}</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
         </div>
       </div>
+      <div className="card-bottom">
+        <button className="wishlist" onClick={addToWishlistHandler}>
+          <span>Wishlist</span>
+        </button>
+        <button className="add-to-cart" onClick={addToCartHandler}>
+          <span>Add to cart</span>
+        </button>
+      </div>
+    </div>
   );
 };
 
